e2987821f118af9f9ceda769255dfed2f92811fe
ПЛАТОН <platon1faton@gmail.com>
2022-09-18 14:28:09 +0300

    1.5

--- c3.py
+++ c3.py
@@ -1,13 +1,11 @@
-import random
 from c1 import Character
 class hil (Character):
     def __init__(self ,name,hp, damage, armor):
         Character.__init__(self, name,hp, damage, armor)
 
-    def hil(self):
+    def hil(self, hp, attack):
         if attack:
-            hilp=random.randint(1.100)
-            if hilp>=70:
-                hils=self.damage*2
-                self.hp+=hils
-                self.damage/2
+            hilp=self.damage*2
+            hp+=hilp
+            self.damage/2
+
--- prover.py
+++ prover.py
@@ -1,15 +1,18 @@
 from c1 import Character
 from c2 import bers
+from c3 import hil
 
 
 player1 = Character(name='Vasya', hp=100, damage=10, armor=0)
 player2 = bers(name='Petya', hp=200, damage=7, armor=0)
+player3 = hil(name='shrez', hp=80, damage=10, armor=0)
 print(player1)
 print(player2)
 
 while player1.hp==0 or player2.hp==0:
-    player1.attack(player2)
-    player2.attack(player1)
+    player1.attack(player2,player3)
+    player2.attack(player1,player3)
+    player2.attack(player1,player2)
     print(player1)
     print(player2)
     print()
